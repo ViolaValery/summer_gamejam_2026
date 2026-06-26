4c9f1c8602a65941c2e1dcf8e5f1e2d6cd41a7e0
Viola Graf <valery.graf@rwth-aachen.de>
2026-06-26 23:07:07 +0200

    renaming timeline, dialogue extended

--- scripts/intro_dialogue.gd
+++ scripts/intro_dialogue.gd
@@ -1,12 +1,20 @@
 extends Node2D
-const timeline = preload("res://timelines/basic_timeline.dtl")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start(timeline)
+	Dialogic.signal_event.connect(DialogicSignal)
+	Dialogic.start("basic_timeline")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta):
 	pass
+
+func DialogicSignal(argument:String):
+	if argument == "quit":
+		get_tree().quit()
+	elif argument == "scene_mainmenu":
+		print("scene_mainmenu")
+		
+		
