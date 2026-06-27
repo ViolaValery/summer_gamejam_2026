d324323484b0348be0b5e00ffa55d921e64e30e3
bmvd70 <bastian.vidal.se@gmail.com>
2026-06-27 23:55:04 +0200

    fixed start to level after intro

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,17 @@
 extends Control
 
+@onready
+var button_start = $start
+@onready
+var button_quit = $quit
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
 	print("MAINMENU READY, visible: ", visible)
 	print("MAINMENU parent: ", get_parent())
+	button_start.pressed.connect(_on_start_pressed)
+	button_quit.pressed.connect(_on_quit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
@@ -13,7 +20,7 @@ func _process(_delta) -> void:
 func _on_start_pressed() -> void:
 	#Global.change_gui_scene("res://scenes/overlay?")
 	print("START PRESSED")
-	Global.game_controller.change_2d_scene("res://scenes/level.tscn")
+	Global.game_controller.change_gui_scene("res://scenes/level.tscn")
 
 func _on_quit_pressed() -> void:
 	print("QUIT PRESSED")
