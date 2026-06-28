09609a8a0f2b69d969b379d03c90f7f294ecbb9a
Ivan Zlatin <zlatinivan1@gmail.com>
2026-06-28 16:50:33 +0200

    acceleration

---
+++ scripts/acceleration.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var push_force := Vector2(500, 0)
+
+func _physics_process(delta):
+	for body in get_overlapping_bodies():
+		if body is RigidBody2D:
+			body.apply_central_force(push_force)
+		elif body is CharacterBody2D:
+			body.velocity += push_force * delta
+			body.move_and_slide()
